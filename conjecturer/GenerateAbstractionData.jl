3e4a9bbddf7de68a31ed58fe8a5608962d29a647
David K. Zhang <dzhang314@gmail.com>
2025-05-14 15:44:47 -0700

    add CRC32c verification to GenerateAbstractionData.jl

---
+++ conjecturer/GenerateAbstractionData.jl
@@ -0,0 +1,84 @@
+using BFloat16s
+using CRC32c
+
+push!(LOAD_PATH, @__DIR__)
+using FloatAbstractions
+
+@inline Base.fma(x::BFloat16, y::BFloat16, z::BFloat16) =
+    BFloat16(fma(Float32(x), Float32(y), Float32(z)))
+
+
+function generate_abstraction_data(
+    ::Type{A},
+    operation::Symbol,
+    ::Type{T},
+    expected_size::Int,
+    expected_crc::UInt32,
+) where {A<:FloatAbstraction,T<:AbstractFloat}
+
+    abstraction_name = string(A)
+    @assert endswith(abstraction_name, "Abstraction")
+    abstraction_name = abstraction_name[begin:end-length("Abstraction")]
+
+    file_name = "$abstraction_name-$operation-$T.bin"
+    if !isfile(file_name)
+        println("Generating $file_name...")
+        flush(stdout)
+        if operation === :TwoSum
+            open(file_name, "w") do io
+                write(io, two_sum_abstractions(A, T))
+            end
+        elseif operation === :TwoProd
+            open(file_name, "w") do io
+                write(io, two_prod_abstractions(A, T))
+            end
+        else
+            error("Unknown operation: $operation (expected :TwoSum or :TwoProd)")
+        end
+    end
+
+    println("Verifying $file_name...")
+    flush(stdout)
+    @assert isfile(file_name)
+    fsize = filesize(file_name)
+    if operation === :TwoSum
+        @assert fsize === expected_size * sizeof(TwoSumAbstraction{A})
+    elseif operation === :TwoProd
+        @assert fsize === expected_size * sizeof(TwoProdAbstraction{A})
+    else
+        error("Unknown operation: $operation (expected :TwoSum or :TwoProd)")
+    end
+    @assert open(crc32c, file_name) === expected_crc
+
+    println("Generated and verified $file_name.")
+    flush(stdout)
+    return nothing
+
+end
+
+
+################################################################# SE ABSTRACTION
+
+
+generate_abstraction_data(SEAbstraction, :TwoSum, Float16, 38_638, 0x18557287)
+generate_abstraction_data(SEAbstraction, :TwoProd, Float16, 62_524, 0x194E7F4D)
+generate_abstraction_data(SEAbstraction, :TwoSum, BFloat16, 548_026, 0xB20B9481)
+generate_abstraction_data(SEAbstraction, :TwoProd, BFloat16, 6_053_588, 0x89B01463)
+
+
+############################################################### SETZ ABSTRACTION
+
+
+generate_abstraction_data(SETZAbstraction, :TwoSum, Float16, 3_833_700, 0x66E6D552)
+generate_abstraction_data(SETZAbstraction, :TwoProd, Float16, 11_454_024, 0x8182FE97)
+generate_abstraction_data(SETZAbstraction, :TwoSum, BFloat16, 26_618_866, 0x1DB442CF)
+generate_abstraction_data(SETZAbstraction, :TwoProd, BFloat16, 313_420_440, 0x897409CD)
+
+
+############################################################# SELTZO ABSTRACTION
+
+
+generate_abstraction_data(SELTZOAbstraction, :TwoSum, Float16, 319_985_950, 0xCC55FA4F)
+generate_abstraction_data(SELTZOAbstraction, :TwoProd, Float16, 1_273_864_440, 0x00AD2611)
+generate_abstraction_data(SELTZOAbstraction, :TwoSum, BFloat16, 1_172_449_766, 0xCB0D263C)
+# generate_abstraction_data(SELTZOAbstraction, :TwoProd, BFloat16, ?, ?)
