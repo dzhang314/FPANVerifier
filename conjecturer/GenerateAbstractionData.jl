446dc6edae89c89879c8d38d31861a42531e69a1
David K. Zhang <dzhang314@gmail.com>
2025-05-14 15:54:05 -0700

    add .gitignore and execution guard

--- conjecturer/GenerateAbstractionData.jl
+++ conjecturer/GenerateAbstractionData.jl
@@ -57,28 +57,32 @@ function generate_abstraction_data(
 end
 
 
-################################################################# SE ABSTRACTION
+if abspath(PROGRAM_FILE) == @__FILE__
 
+    generate_abstraction_data(SEAbstraction, :TwoSum, Float16,
+        38_638, 0x18557287)
+    generate_abstraction_data(SEAbstraction, :TwoSum, BFloat16,
+        548_026, 0xB20B9481)
+    generate_abstraction_data(SEAbstraction, :TwoProd, Float16,
+        62_524, 0x194E7F4D)
+    generate_abstraction_data(SEAbstraction, :TwoProd, BFloat16,
+        6_053_588, 0x89B01463)
 
-generate_abstraction_data(SEAbstraction, :TwoSum, Float16, 38_638, 0x18557287)
-generate_abstraction_data(SEAbstraction, :TwoProd, Float16, 62_524, 0x194E7F4D)
-generate_abstraction_data(SEAbstraction, :TwoSum, BFloat16, 548_026, 0xB20B9481)
-generate_abstraction_data(SEAbstraction, :TwoProd, BFloat16, 6_053_588, 0x89B01463)
+    generate_abstraction_data(SETZAbstraction, :TwoSum, Float16,
+        3_833_700, 0x66E6D552)
+    generate_abstraction_data(SETZAbstraction, :TwoSum, BFloat16,
+        26_618_866, 0x1DB442CF)
+    generate_abstraction_data(SETZAbstraction, :TwoProd, Float16,
+        11_454_024, 0x8182FE97)
+    generate_abstraction_data(SETZAbstraction, :TwoProd, BFloat16,
+        313_420_440, 0x897409CD)
 
+    generate_abstraction_data(SELTZOAbstraction, :TwoSum, Float16,
+        319_985_950, 0xCC55FA4F)
+    generate_abstraction_data(SELTZOAbstraction, :TwoSum, BFloat16,
+        1_172_449_766, 0xCB0D263C)
+    generate_abstraction_data(SELTZOAbstraction, :TwoProd, Float16,
+        1_273_864_440, 0x00AD2611)
+    # generate_abstraction_data(SELTZOAbstraction, :TwoProd, BFloat16, ?, ?)
 
-############################################################### SETZ ABSTRACTION
-
-
-generate_abstraction_data(SETZAbstraction, :TwoSum, Float16, 3_833_700, 0x66E6D552)
-generate_abstraction_data(SETZAbstraction, :TwoProd, Float16, 11_454_024, 0x8182FE97)
-generate_abstraction_data(SETZAbstraction, :TwoSum, BFloat16, 26_618_866, 0x1DB442CF)
-generate_abstraction_data(SETZAbstraction, :TwoProd, BFloat16, 313_420_440, 0x897409CD)
-
-
-############################################################# SELTZO ABSTRACTION
-
-
-generate_abstraction_data(SELTZOAbstraction, :TwoSum, Float16, 319_985_950, 0xCC55FA4F)
-generate_abstraction_data(SELTZOAbstraction, :TwoProd, Float16, 1_273_864_440, 0x00AD2611)
-generate_abstraction_data(SELTZOAbstraction, :TwoSum, BFloat16, 1_172_449_766, 0xCB0D263C)
-# generate_abstraction_data(SELTZOAbstraction, :TwoProd, BFloat16, ?, ?)
+end
