3b8c072d4b4f0f0e887175fbe835a854e3e392e3
David K. Zhang <dzhang314@gmail.com>
2025-08-21 03:55:03 -0700

    change tmp dir to smt2

--- smt_utils.py
+++ smt_utils.py
@@ -310,9 +310,8 @@ def create_smt_job(
     contents: str = f"(set-logic {logic})\n" + solver.to_smt2()
     solver.pop()
 
-    # Write SMT-LIB 2 file in tmp subdirectory.
-    os.makedirs("tmp", exist_ok=True)
-    filename: str = os.path.join("tmp", name + ".smt2")
+    os.makedirs("smt2", exist_ok=True)
+    filename: str = os.path.join("smt2", name + ".smt2")
     with open(filename, "w") as f:
         _ = f.write(contents)
 
